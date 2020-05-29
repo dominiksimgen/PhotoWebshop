3245bba3adf71e0841367d3a6c2c58090a8dca80
dominiksimgen <simgen.d@googlemail.com>
2020-05-29 19:03:54 +0200

    source code cleaned

--- script.js
+++ script.js
@@ -1,88 +1,72 @@
 //dynamically insert the HTML for the image pane
-
+//declaring global variables
 var styleSheet = "";
 var collection = "j";
 var cartArray = [];
-var imageCountPerColumn = Math.floor(21 / 3); //Idea for improvement: find way to scale the algorithm dynamically, if new pictures are uploaded
-
+var imageCountPerColumn = Math.floor(21 / 3); 
+//Idea for improvement: find way to scale the algorithm dynamically, if new pictures are uploaded
 
 function drawImagePane() {
-var column1 = "";
-var column2 = "";
-var column3 = "";
-  for (let i = 1; i <= imageCountPerColumn; i++) {
-    column1 +=
-      '<div class="container"><img src="img/' +
-      collection +
-      i +
-      '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
-    column2 +=
-      '<div class="container"><img src="img/' +
-      collection +
-      (i + imageCountPerColumn) +
-      '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
-    column3 +=
-      '<div class="container"><img src="img/' +
-      collection +
-      (i + imageCountPerColumn + imageCountPerColumn) +
-      '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
-  }
-  document.getElementsByClassName("column")[0].innerHTML = column1;
-  document.getElementsByClassName("column")[1].innerHTML = column2;
-  document.getElementsByClassName("column")[2].innerHTML = column3;
-  var column1 = "<div></div>";
+  var column1 = "";
   var column2 = "";
   var column3 = "";
+    for (let i = 1; i <= imageCountPerColumn; i++) {
+      column1 +=
+        '<div class="container"><img src="img/' +
+        collection +
+        i +
+        '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
+      column2 +=
+        '<div class="container"><img src="img/' +
+        collection +
+        (i + imageCountPerColumn) +
+        '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
+      column3 +=
+        '<div class="container"><img src="img/' +
+        collection +
+        (i + imageCountPerColumn + imageCountPerColumn) +
+        '.jpeg"><div class="content"><button id="myBtn" class="ViewButton">View</button><button class="AddToCartButton">Add to cart</button></div></div>';
+    }
+    document.getElementsByClassName("column")[0].innerHTML = column1;
+    document.getElementsByClassName("column")[1].innerHTML = column2;
+    document.getElementsByClassName("column")[2].innerHTML = column3;
 
-// Get Modal elements
-var modal = document.getElementById("myModal");
-//assign EventListener and function to each view button
-var numberOfViewButtons = document.querySelectorAll(".ViewButton").length;
-for (let i = 0; i < numberOfViewButtons; i++) {
-  document.querySelectorAll(".ViewButton")[i].addEventListener("click", function () {
+  // Get image modal elements
+  var modal = document.getElementById("myModal");
+  //assign EventListener and function to each view button
+  var numberOfViewButtons = document.querySelectorAll(".ViewButton").length;
+  for (let i = 0; i < numberOfViewButtons; i++) {
+    document.querySelectorAll(".ViewButton")[i].addEventListener("click", function () {
+        var pictureAdress = this.parentNode.parentNode.children[0].getAttribute(
+          "src"
+        );
+        //console.log(pictureAdress);
+        document.getElementById("modalImage").setAttribute("src", pictureAdress);
+        modal.style.display = "block";
+      });
+  }
+  //assign modal close functionality
+  var modalCloseButton = document.getElementsByClassName("close")[0];
+  modalCloseButton.onclick = function () {  // When the user clicks on <span> (x), close the modal
+    modal.style.display = "none";
+  };
+  window.onclick = function (event) { // When the user clicks anywhere outside of the modal, close it
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+  //add to cart button
+  var numberOfAddToCartButtons = document.querySelectorAll(".AddToCartButton").length;
+  for(let i = 0; i < numberOfAddToCartButtons; i++ ) {
+    document.querySelectorAll(".AddToCartButton")[i].addEventListener("click", function() {
       var pictureAdress = this.parentNode.parentNode.children[0].getAttribute(
         "src"
       );
-      //console.log(pictureAdress);
-      document.getElementById("modalImage").setAttribute("src", pictureAdress);
-      modal.style.display = "block";
+      cartArray.push(pictureAdress);
+      refreshShoppingCart();
     });
-}
-//assign close functionality
-var modalCloseButton = document.getElementsByClassName("close")[0];
-modalCloseButton.onclick = function () {  // When the user clicks on <span> (x), close the modal
-  modal.style.display = "none";
-};
-window.onclick = function (event) { // When the user clicks anywhere outside of the modal, close it
-  if (event.target == modal) {
-    modal.style.display = "none";
   }
-};
-
-//add to cart button
-var numberOfAddToCartButtons = document.querySelectorAll(".AddToCartButton").length;
-for(let i = 0; i < numberOfAddToCartButtons; i++ ) {
-  document.querySelectorAll(".AddToCartButton")[i].addEventListener("click", function() {
-    var pictureAdress = this.parentNode.parentNode.children[0].getAttribute(
-      "src"
-    );
-    cartArray.push(pictureAdress);
-    refreshShoppingCart();
-    /* local storage not working, implement later:
-    localStorage.setItem("storedCart", JSON.stringify(cartArray));
-    var retrievedData = localStorage.getItem("storedCart");
-    var cartArray2 = JSON.parse(retrievedData);
-    console.log(cartArray);
-    */
-  });
 }
-
-
-}
-
-
-
-
 
 //refresh shopping cart
 function refreshShoppingCart() {
@@ -102,7 +86,7 @@ function refreshShoppingCart() {
   }
 }
 
-////assign EventListener and function to each "remove" button
+//assign EventListener and function to each "remove" button
 function activateCartItemRemoveButton() {
   var numberOfCartItemRemoveButtons = document.querySelectorAll(".cartItemRemoveButton").length;
   for(let i = 0; i < numberOfCartItemRemoveButtons; i++ ) {
@@ -110,7 +94,6 @@ function activateCartItemRemoveButton() {
       var cartPictureAdress = this.parentNode.children[0].getAttribute("src");
       for ( let i = 0; i < cartArray.length; i++) {
         if (cartArray[i] === cartPictureAdress ){
-          //console.log(cartPictureAdress);
           cartArray.splice(i, 1);
           break;
         }
@@ -125,26 +108,22 @@ function activateCartItemRemoveButton() {
       cartArray = [];
       refreshShoppingCart();
       document.getElementsByClassName("orderComplete")[0].style.display = "block";
-    }
-    )
+    })
  }
 
-
-
+ //open and close shopping cart
 var cart = document.getElementsByClassName("cart")[0];
-//console.log(cart);
 var cartOpenButton = document.getElementById("cartOpenButton");
 cartOpenButton.onclick = function () {
   refreshShoppingCart();
   cart.style.display = "block";
 }
-
-
 var cartCloseButton = document.getElementsByClassName("cartClose")[0];
 cartCloseButton.onclick = function () { 
   cart.style.display = "none";
 };
 
+//open a collection
 var japanButton = document.getElementsByClassName("sidebarButtons")[2];
 japanButton.onclick = function () {
   collection = "j";
@@ -152,9 +131,7 @@ japanButton.onclick = function () {
   document.getElementsByTagName("link")[0].setAttribute("href", styleSheet);
   drawImagePane();
 }
-
 var scotlandButton = document.getElementsByClassName("sidebarButtons")[3];
-
 scotlandButton.onclick = function () {
   collection = "s";
   styleSheet = "stylescotland.css";
